d72a068103acf0359f4340aaf6190f2f00a4417b
Mhayk Whandson <hi@mhayk.com>
2020-08-05 12:43:45 +0100

    A component name PageHeader has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+interface PageHeaderProps {
+  title: string;
+}
+
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
+
+  const { title } = props;
+  return (
+    <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Back"/>
+          </Link>
+          <img src={logoImg} alt="Proffy"/>
+        </div>
+
+        <div className="header-content">
+          <strong>{title}</strong>
+        </div>
+      </header>
+  )
+}
+
+export default PageHeader;
