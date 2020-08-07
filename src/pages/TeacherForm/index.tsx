79cc8469896b8b6a4d8b40e8807d28870664ece6
Mhayk Whandson <hi@mhayk.com>
2020-08-07 22:25:38 +0100

    A new props named descriptions has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -5,7 +5,10 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="How amazing you want to teach" />
+      <PageHeader
+        title="How amazing you want to teach"
+        description="The first step is to fill out this application form."
+       />
     </div>
   )
 }
