d19596e60cce31ad542babc91a4ff630a3804021
Mhayk Whandson <hi@mhayk.com>
2020-08-08 10:56:48 +0100

    The TeacherForm page's style to mobile screen is ready.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
@@ -9,6 +14,32 @@ function TeacherForm() {
         title="How amazing you want to teach"
         description="The first step is to fill out this application form."
        />
+
+       <main>
+         <fieldset>
+           <legend>Your informations</legend>
+          <Input name="name" label="Full name" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+         </fieldset>
+
+         <fieldset>
+           <legend>About the class</legend>
+          <Input name="subject" label="Subject" />
+          <Input name="cost" label="Cost your hour per class" />
+         </fieldset>
+
+         <footer>
+           <p>
+             <img src={warningIcon} alt="Important warning"/>
+             important! <br />
+             Fill all the fields
+           </p>
+           <button type="button">
+             Save
+           </button>
+         </footer>
+       </main>
     </div>
   )
 }
