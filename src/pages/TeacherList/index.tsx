af34a38a510862cdcda967a224bfe80b4dca5f47
Mhayk Whandson <hi@mhayk.com>
2020-08-08 10:39:58 +0100

    The page TeacherList has been refactored to use Input component.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem'
+import Input from '../../components/Input';
 
 import './styles.css';
 
@@ -10,20 +11,9 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="These are the proffys availables.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Subject</label>
-            <input type="text" name="subject" id="subject"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Week Day</label>
-            <input type="text" name="week_day" id="week_day"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Time</label>
-            <input type="text" name="time" id="time"/>
-          </div>
+          <Input name="subject" label="Subject" />
+          <Input name="week_day" label="Week Day" />
+          <Input name="time" label="Time" />
         </form>
       </PageHeader>
       <main>
