2b934e9492913a1231aed85ae9d99d2b2300d1c7
Mhayk Whandson <hi@mhayk.com>
2020-08-16 16:06:46 +0100

    After the class has been created, it is redirect to the url /.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,16 +1,20 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Textarea from '../../components/TextArea';
 import Select from '../../components/Select';
 
-import './styles.css';
-
 import warningIcon from '../../assets/images/icons/warning.svg';
+
 import api from '../../services/apit';
 
+import './styles.css';
+
 function TeacherForm() {
+  const history = useHistory();
+
   const [name, setName] = useState('');
   const [avatar, setAvatar] = useState('');
   const [whatsapp, setWhatsapp] = useState('');
@@ -56,6 +60,8 @@ function TeacherForm() {
       schedule: scheduleItems
     }).then(() => {
       alert('Cadastro realizado com sucesso!');
+
+      history.push('/')
     }).catch(() => {
       alert('Error')
     })
